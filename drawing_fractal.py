18fb408946bad3eeeb706f1f2e0af5456cc01eec
Mathias FARVACQUE <132013078+anonymecrasher@users.noreply.github.com>
2023-09-25 11:44:27 +0200

    Update drawing_fractal.py
    add carre_fractal func
    add courbe kch func

--- drawing_fractal.py
+++ drawing_fractal.py
@@ -1,29 +1,68 @@
 import turtle as t
 
-def arbre(n, l):
-    
-    if n > 0:
+
+def arbre(n, l, An):
+    if n == 0:
         t.forward(l)
-        t.left(45)
+        t.left(180)
+        t.forward(l)
+        t.left(180)
+    elif n > 0:
+        t.forward(l)
+        t.right(An)
+        arbre(n-1, l *(2/3),An)
+        t.left(2*An)
+        arbre(n-1, l *(2/3),An)
+        t.right(An)
+        t.left(180)
+        t.forward(l)
+        t.left(180)
+
+def carre(l):
+    for i in range(4):
+        t.forward(l)
+        t.left(90)
+
+def carre_fractal(n,l):
+    if n == 0:
+        carre(l)
+    elif n > 0:
+        carre_fractal(n-1,l/3)
         t.forward(l/3)
-        t.left(180)
+        carre_fractal(n-1,l/3)
         t.forward(l/3)
-        t.left(180)
-        arbre(n-1,l/3)
-        t.left(180)
+        carre_fractal(n-1,l/3)
+        t.forward(l/3)
         t.left(90)
         t.forward(l/3)
-        t.left(180)
+        carre_fractal(n-1,l/3)
         t.forward(l/3)
-        t.left(180)
-        arbre(n-1,l/3)
-        t.left(45)
+        carre_fractal(n-1,l/3)
+        t.forward(l/3)
+        t.left(90)
+        t.forward(l/3)
+        carre_fractal(n-1,l/3)
+        t.forward(l/3)
+        carre_fractal(n-1,l/3)
+        t.forward(l/3)
+        t.left(90)
+        t.forward(l/3)
+        carre_fractal(n-1,l/3)
+        t.forward(l/3)
+        t.forward(l/3)
+        t.left(90)
+        
+def courbe_de_koch(n, l):
+    if n == 0:
         t.forward(l)
-        t.left(180)
-        
-        
-        
-        
+    elif n > 1:
+        courbe_de_koch(n-1, l/3)
+        t.Left(60)
+        courbe_de_koch(n-1, l/3)
+        t.right(120)
+        courbe_de_koch(n-1, l/3)
+        t.left(60)
+        courbe_de_koch(n-1, l/3)
         
         
         
@@ -31,7 +70,11 @@ def arbre(n, l):
         
         
 def main():
-    t.speed(1)
+    t.speed(0)
     t.left(90)
-    arbre(3, 150)
+    carre_fractal(4,400)
+    t.done
+    
+    
+
 main()
